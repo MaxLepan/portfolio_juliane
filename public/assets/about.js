5a23b9d88e6811e487ca0b4cb4f541b437068833
MaxLepan <max.lepan@laposte.net>
2021-05-06 11:21:25 +0200

    PP bug fix

--- public/assets/about.js
+++ public/assets/about.js
@@ -3,7 +3,7 @@ let img = document.querySelector("#img-container")
 
 const requestUser = async () => {
     //const APILocation = "http://localhost:3000/";
-    const APIlocation = "https://julianestrappazzon.herokuapp.com/";
+    const APILocation = "https://julianestrappazzon.herokuapp.com/";
     
 
     const response = await axios.get(`${APILocation}getUser`);
